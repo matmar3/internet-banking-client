0cfd38abdf43f82de8ddd50f5c9762d5e3bcba10
Martin Matas <nouzovenkonto@gmail.com>
2019-03-26 07:30:58 +0100

    Implemented signUp process + progress dialog

---
+++ app/src/main/java/cz/zcu/kiv/martinm/internetbankingclient/AsyncActivity.java
@@ -0,0 +1,38 @@
+package cz.zcu.kiv.martinm.internetbankingclient;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+
+public abstract class AsyncActivity extends Activity {
+
+    private ProgressDialog progressDialog;
+
+    private boolean destroyed = false;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        destroyed = true;
+    }
+
+    public void showLoadingProgressDialog() {
+        this.showProgressDialog("Loading. Please wait...");
+    }
+
+    public void showProgressDialog(CharSequence message) {
+        if (progressDialog == null) {
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setIndeterminate(true);
+        }
+
+        progressDialog.setMessage(message);
+        progressDialog.show();
+    }
+
+    public void dismissProgressDialog() {
+        if (progressDialog != null && !destroyed) {
+            progressDialog.dismiss();
+        }
+    }
+
+}
--- app/src/main/java/cz/zcu/kiv/martinm/internetbankingclient/MainActivity.java
+++ app/src/main/java/cz/zcu/kiv/martinm/internetbankingclient/MainActivity.java
@@ -1,13 +1,61 @@
 package cz.zcu.kiv.martinm.internetbankingclient;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import cz.zcu.kiv.martinm.internetbankingclient.client.InternetBankingRESTClient;
+
+public class MainActivity extends AsyncActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StrictMode.ThreadPolicy policy = new
+        StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        // Initiate the request to the protected service
+        final Button submitButton = findViewById(R.id.btn_signup);
+        submitButton.setOnClickListener(v -> new SignUpTask().execute());
     }
+
+    private void displayResponse(Boolean response) {
+        Toast.makeText(this, response.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    private class SignUpTask extends AsyncTask<Void, Void, Boolean> {
+
+        private String username;
+
+        private String password;
+
+        @Override
+        protected void onPreExecute() {
+            showLoadingProgressDialog();
+
+            EditText editText = findViewById(R.id.username);
+            this.username = editText.getText().toString();
+
+            editText = findViewById(R.id.password);
+            this.password = editText.getText().toString();
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            return InternetBankingRESTClient.getInstance().signUp(username, password);
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            dismissProgressDialog();
+            if (result != null) displayResponse(result);
+        }
+
+    }
+
 }
